b47be6d9669eb322602c50009a05955a96c7225a
Andriy <andrysimonyak@gmail.com>
2020-11-06 18:41:35 +0200

    lab 5 1st player values

--- labs/src/main/java/com/shymoniak/lab5/TaskL5.java
+++ labs/src/main/java/com/shymoniak/lab5/TaskL5.java
@@ -8,6 +8,7 @@ import com.shymoniak.tools.FileWorker;
 import com.shymoniak.tools.MatrixActions;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class TaskL5 {
     public void run() {
@@ -17,66 +18,91 @@ public class TaskL5 {
         matrixActions.print(matrix);
 
         MatrixGames matrixGames = new MatrixGames();
-        if (matrixGames.isSolutionSaddlePoint(matrix) !=-1){
+        if (matrixGames.isSolutionSaddlePoint(matrix) != -1) {
             System.out.println("Saddle point" + matrixGames.isSolutionSaddlePoint(matrix));
         } else {
             System.out.println("Mixed strategy");
             matrix = matrixGames.checkDominationRows(matrix);
             matrix = matrixGames.checkDominationCols(matrix);
+            matrix = addAdditionalMatrixRow(matrix);
 
             LinearSystem<Float, MyEquation> list = generateSystem(matrix);
             printSystem(list);
-//            int i, j;
-//            Algorithm<Float, MyEquation> alg = new Algorithm<Float, MyEquation>(list);
-//            try{
-//                alg.calculate();
-//            }catch (NullPointerException e){
-//                System.out.println(e.getMessage());
-//                System.exit(0);
-//            }catch (ArithmeticException e){
-//                System.out.println(e.getMessage());
-//                System.exit(0);
-//            }
-//            Float [] x = new Float[DEFAULT_EQUATIONS_NUMBER];
-//            for(i = list.size() - 1; i >= 0; i--) {
-//                Float sum = 0.0f;
-//                for(j = list.size() - 1; j > i; j--) {
-//                    sum += list.itemAt(i, j) * x[j];
-//                }
-//                x[i] = (list.itemAt(i, list.size()) - sum) / list.itemAt(i, j);
-//            }
-//            printSystem(list);
-//            printVector(x);
+            int i, j;
+            Algorithm<Float, MyEquation> alg = new Algorithm<>(list);
+            try {
+                alg.calculate();
+            } catch (NullPointerException e) {
+                System.out.println(e.getMessage());
+                System.exit(0);
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+                System.exit(0);
+            }
+            Float[] x = new Float[matrix.length];
+            for (i = list.size() - 1; i >= 0; i--) {
+                Float sum = 0.0f;
+                for (j = list.size() - 1; j > i; j--) {
+                    sum += list.itemAt(i, j) * x[j];
+                }
+                x[i] = (list.itemAt(i, list.size()) - sum) / list.itemAt(i, j);
+            }
+            printSystem(list);
+            printVector(x);
         }
 
     }
 
-    public static LinearSystem<Float, MyEquation> generateSystem(int[][] arr){
+    private static LinearSystem<Float, MyEquation> generateSystem(int[][] arr) {
         LinearSystem<Float, MyEquation> list = new LinearSystem<Float, MyEquation>();
         int i;
-        for (i = 0; i < arr.length; i++){
+        for (i = 0; i < arr.length; i++) {
             MyEquation equation = new MyEquation();
-            equation.generate(arr);
+            equation.generate(arr[i]);
             list.push(equation);
         }
         return list;
     }
 
-    public static void printSystem(LinearSystem<Float, MyEquation> system){
-        for (int i = 0; i < system.size(); i++){
+    private static void printSystem(LinearSystem<Float, MyEquation> system) {
+        for (int i = 0; i < system.size(); i++) {
             MyEquation temp = system.get(i);
             String s = "";
-            for (int j = 0; j < temp.size(); j++){
+            for (int j = 0; j < temp.size(); j++) {
                 s += String.format("%f; %s", system.itemAt(i, j), "\t");
             }
             System.out.println(s);
-        }System.out.println("");
+        }
+        System.out.println("");
     }
 
-    public static void printVector(Float [] x){
+    private static void printVector(Float[] x) {
         String s = "";
-        for (int i = 0; i < x.length; i++){
+        for (int i = 0; i < x.length; i++) {
             s += String.format("x%d = %f; ", i, x[i]);
-        }System.out.println(s);
+        }
+        System.out.println(s);
+    }
+
+    // Додаємо додаткове значення х5 до кожного рядка з коефіцієнтом -1, прирівнюєм кожне рівняння до 0
+    // x1 + x2 +...+ xn - y = 0
+    // Додаємо додатковий рядок  x1 + x2 +...+ xn + 0y = 1
+    private int[][] addAdditionalMatrixRow(int[][] arr) {
+        int[][] newArr = new int[arr.length + 1][arr[0].length + 2];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                newArr[i][j] = arr[i][j];
+            }
+        }
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i][newArr[0].length - 2] = -1;
+            newArr[i][newArr[0].length - 1] = 0;
+        }
+        for (int i = 0; i < arr[0].length; i++) {
+            newArr[newArr.length - 1][i] = 1;
+        }
+        newArr[newArr.length - 1][newArr.length - 1] = 0;
+        newArr[newArr.length - 1][newArr.length] = 1;
+        return newArr;
     }
 }
